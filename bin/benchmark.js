7d4a4ac4b60c9a1f8deb43ce95ae4667c7fd8365
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-04-06 23:07:45 -0700

    lodash templated version of FFT

--- bin/benchmark.js
+++ bin/benchmark.js
@@ -3,11 +3,11 @@
 'use strict';
 
 var Benchmark = require('benchmark'),
-    fourier = require('../lib');
+    fft = require('../lib/fft-custom.js');
 
 var suite = new Benchmark.Suite;
 
-var re, im, j;
+var re, im, j, fn = fft();
 
 function init (N) {
     return function () {
@@ -33,31 +33,30 @@ function initType (N, Type) {
     };
 }
 
-function fft () {
-    var fn = fourier.fft();
-    return function () {
-        fn(re, im);
-    };
-}
-
 for (j = 16; j <= Math.pow(2, 19); j *= 2) {
     suite.add('fft-single-' + j, {
         onStart: initType(j, Float32Array),
-        fn: fft()
+        fn: (function (N) {
+            return function () { fn['fft' + N](re, im); };
+        })(j)
     });
 }
 
 for (j = 16; j <= Math.pow(2, 19); j *= 2) {
     suite.add('fft-double-' + j, {
         onStart: initType(j, Float64Array),
-        fn: fft()
+        fn: (function (N) {
+            return function () { fn['fft' + N](re, im); };
+        })(j)
     });
 }
 
 for (j = 16; j <= Math.pow(2, 19); j *= 2) {
     suite.add('fft-' + j, {
         onStart: init(j),
-        fn: fft()
+        fn: (function (N) {
+            return function () { fn['fft' + N](re, im); };
+        })(j)
     });
 }
 
