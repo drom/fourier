6da4146354d170193f889dea3d55cbb3cc7d8482
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-03-10 22:26:38 -0800

    added eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "ArrayBuffer": true,
+        "Float64Array": true,
+        "Float32Array": true
+    }
+};
