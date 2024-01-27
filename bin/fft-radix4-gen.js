653a05515eb86faa4ec33eae6046268cb0302a8a
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2024-01-26 23:18:34 -0800

    var -> let, const

--- bin/fft-radix4-gen.js
+++ bin/fft-radix4-gen.js
@@ -2,16 +2,16 @@
 
 'use strict';
 
-var template = require('lodash.template'),
-  fs = require('fs'),
-  path = require('path');
+const template = require('lodash.template');
+const fs = require('fs');
+const path = require('path');
 
 fs.readFile(
   path.resolve(__dirname, '../src/fft-radix4-template.js'),
   { encoding: 'utf8' },
   function (err, data) {
     if (err) { throw err; }
-    var t = template(data);
+    const t = template(data);
     console.log(t({
       asms: [false],
       bases: [3],
